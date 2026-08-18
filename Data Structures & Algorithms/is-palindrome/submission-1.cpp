b59df61e72e4726cdd8979f81c727fe90abe7af2
Ajay Patel <81469239+Ajay051839@users.noreply.github.com>
2026-08-18 07:30:24 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start=0,end=s.size()-1;
+        while(start<=end && end>=start){
+            if(!isalnum(s[start])){
+                start++;
+            }
+            if(!isalnum(s[end])){
+                end--;
+            }
+            if(isalnum(s[start]) && isalnum(s[end])){
+                if(tolower(s[start])!=tolower(s[end])){
+                    return false;
+                }else{
+                    start++;
+                    end--;
+                }
+            }
+        }
+        return true;
+    }
+};
