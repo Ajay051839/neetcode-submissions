2f45ff0fbe58032cd6aa4712e59c0cda5e1b796c
Ajay Patel <81469239+Ajay051839@users.noreply.github.com>
2026-08-24 08:18:24 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+bool dfs(int node,vector<int> &vis, vector<int> &pathVis,const vector<int> adj[], stack<int> &st){
+    vis[node]=1;
+    pathVis[node]=1;
+    for(auto i:adj[node]){
+        if(!vis[i]){
+            if(dfs(i,vis,pathVis,adj,st))return true;
+        }else if(pathVis[i]){
+            return true;
+        }
+    }
+    pathVis[node]=0;
+    st.push(node);
+    return false;
+}    
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& preReq) {
+        vector<int> adj[n];
+        for(auto i:preReq){
+            adj[i[1]].push_back(i[0]);
+        }
+        vector<int>vis(n,0);
+        vector<int>pathVis(n,0);
+        stack<int>st;
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                if(dfs(i,vis,pathVis,adj,st))return {};
+            }
+        }
+        
+        if(st.size()!=n)return {};
+        vector<int>ans;
+        while(!st.empty()){
+            int node=st.top();
+            st.pop();
+            ans.push_back(node);
+        }
+        return ans;
+    }
+};
