f0f75497e1cdb2929dbe7c161edce6fe6018a94c
Ajay Patel <81469239+Ajay051839@users.noreply.github.com>
2026-08-29 07:57:16 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> leftmx(n);
+        leftmx[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftmx[i]=max(leftmx[i-1],height[i]);
+        }
+        vector<int>rightmx(n);
+        rightmx[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+           rightmx[i]=max(rightmx[i+1],height[i]);
+        }
+        int water=0;
+        for(int i=0;i<n;i++){
+            water+=(min(leftmx[i],rightmx[i])-height[i]);
+        }
+        return water;
+
+    }
+};
